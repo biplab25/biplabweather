e9029bd3d56334f64ae66344763093d581817272
Biplab Bhattacharya <biplabmath1998@gmail.com>
2021-05-03 19:29:05 +0530

    All file changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input=document.querySelector("input");
-const search=document.querySelector("button");
+const search=document.querySelector(".third");
 const show=document.querySelector(".show");
 const h1temp=document.querySelector("#temp");
 let inputValue = document.querySelector("#myInput");
@@ -10,23 +10,25 @@ const error=document.querySelector("#error");
 // 
 /* Calling the function after search button click*/
 search.addEventListener("click",async function(){
-      await  getInformation(inputValue.value);
-});
-
-/* Start of getInformation of weather function */
-async function getInformation(arg){
         try{
-        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${arg}&units=metric&appid=6a96533f811b24ebc00ff8c9d665e0b4`);
-        const data=await response.json();
-        console.log(data);
-        h1temp.innerHTML=data.main.temp+'°C';
-        h1location.innerHTML=data.name;
-        description.innerHTML= data.weather[0].main;
-        icon.src="https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+        await  getInformation(inputValue.value);
         }
         catch(erro){
                 alert("Whoops! No City Was Found");
         }
+});
+
+/* Start of getInformation of weather function */
+async function getInformation(arg){
+
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${arg}&units=metric&appid=6a96533f811b24ebc00ff8c9d665e0b4`);
+        const data=await response.json();
+        //console.log(data);
+        h1temp.innerHTML=data.main.temp+'°C';
+        h1location.innerHTML=data.name;
+        description.innerHTML= data.weather[0].main;
+        icon.src="https://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+        inputValue.value="";
 }
 /* End of getInformation of weather function */
 
